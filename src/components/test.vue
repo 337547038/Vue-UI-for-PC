3017015fbe0531cebc2cc0abdd1a188028394928
337547038 <33547038@qq.com>
2017-12-05 22:37:37 +0800

    12.5

---
+++ src/components/test.vue
@@ -0,0 +1,20 @@
+<!--Created by 337547038 on 2017/12/5.-->
+<template>
+    <div>
+test
+    </div>
+</template>
+<script>
+    export default {
+        name: '',
+        data(){
+            return {}
+        },
+        props: [],
+        mounted(){
+        },
+        components: {},
+        methods: {},
+        computed: {}
+    }
+</script>
---
+++ src/pages/test.vue
@@ -0,0 +1,21 @@
+<!--Created by 337547038 on 2017/12/5.-->
+<template>
+    <div>
+000
+        <Test></Test>
+    </div>
+</template>
+<script>
+    import Test from '../components/test'
+    export default {
+        name: 'test',
+        path: '/test',
+        data(){
+            return {}
+        },
+        mounted(){
+        },
+        components: {Test},
+        methods: {}
+    }
+</script>
