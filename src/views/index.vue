968c8fe67af85f6c674a607f4711bc5280887983
337547038 <337547038@qq.com>
2019-11-11 17:35:10 +0800

    update

---
+++ src/views/index.vue
@@ -0,0 +1,26 @@
+<!-- Created by 337547038 on 2018/8/10 0010. -->
+<template>
+  <div>
+    <h1 style="text-align: center;margin: 50px auto">组件doc及示例演示请运行 npm run docs</h1>
+    <p style="text-align: center">
+      <router-link to="/login">应用实例</router-link>
+    </p>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+
+  },
+  mounted () {
+
+  }
+}
+</script>
