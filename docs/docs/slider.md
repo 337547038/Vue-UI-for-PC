f2d0eeeb4f352797f9c761e9df52db5972b266f8
337547038 <337547038@qq.com>
2019-11-16 20:52:23 +0800

    add slider

--- docs/docs/slider.md
+++ docs/docs/slider.md
@@ -1,1 +1,196 @@
 <!-- Created by 337547038 on 2019/11/15 0015. -->
+# Slider 滑块
+<script>
+ export default {
+   data(){
+     return {
+       value1:0,
+       value2:50,
+       value3:25,
+       value4:25,
+       value5:30,
+       value6:30,
+       value7:30,
+       value8:[30,60],
+       value9:[30,50],
+       value10:80,
+       marks: {
+          0: 'start',
+          25: '25%',
+          50: 'center',
+          75: '75%',
+          100: '<i style="color:red">end</i>'
+        }
+     }
+   },
+   methods:{
+     formatTooltip(value){
+       return value+'%'
+     }
+   }
+ }
+</script>
+### 基础用法
+:::demo 
+```html
+<template>
+  <div>
+  
+    <p>基础用法：{{value1}}</p>
+    <div><ak-slider v-model="value1"></ak-slider></div>
+    
+    <p>隐藏提示：{{value2}}</p>
+    <div><ak-slider v-model="value2" :showTooltip="false"></ak-slider></div>
+    
+    <p>格式化提示：{{value3}}</p>
+    <div><ak-slider v-model="value3" :formatTooltip="formatTooltip"></ak-slider></div>
+    
+    <p>禁用状态：{{value4}}</p>
+    <div><ak-slider v-model="value4" :disabled="true"></ak-slider></div>
+    
+  </div>
+</template>
+<script>
+ export default {
+   data(){
+     return {
+       value1:0,
+       value2:50,
+       value3:25,
+       value4:25
+     }
+   },
+   methods:{
+     formatTooltip(value){
+       return value+'%'
+     }
+   }
+ }
+</script>
+```
+:::
+
+### 指定滑动步长
+:::demo 步长`step=10`
+```html
+<template>
+  <div>
+  
+    <p>指定滑动步长：{{value5}}</p>
+    <div><ak-slider v-model="value5" :step="10"></ak-slider></div>
+    
+    <p>显示间断点：{{value6}}</p>
+    <div><ak-slider v-model="value6" :step="10" :showStops="true"></ak-slider></div>
+    
+  </div>
+</template>
+<script>
+ export default {
+   data(){
+     return {
+       value5:30,
+       value6:30
+     }
+   }
+ }
+</script>
+```
+:::
+
+### 设置最小值和最大值
+:::demo `min`最小值10，`max`最大值80
+```html
+<template>
+  <div>
+  
+    <p>指定最大值和最小值：{{value7}}</p>
+    <div><ak-slider v-model="value7" :min="10" :max="80"></ak-slider></div>
+    
+  </div>
+</template>
+<script>
+ export default {
+   data(){
+     return {
+       value7:30,
+     }
+ }
+</script>
+```
+:::
+
+### 设定选择范围
+:::demo 通用`value`传入两个值即可
+```html
+<template>
+  <div>
+  
+    <p>选择范围{{value8}}</p>
+    <div><ak-slider v-model="value8"></ak-slider></div>
+    
+    <p>选择范围，并指定最大最小值{{value9}}</p>
+    <div><ak-slider v-model="value9" :min="10" :max="80"></ak-slider></div>
+    
+  </div>
+</template>
+<script>
+ export default {
+   data(){
+     return {
+       value8:[30,60],
+       value9:[30,50]
+     }
+   }
+ }
+</script>
+```
+:::
+
+### 展示标记
+:::demo key表示当前所占百分比位置
+```html
+<template>
+  <div>
+  
+    <p>展示标记{{value10}}</p>
+    <div><ak-slider v-model="value10" :marks="marks"></ak-slider></div>
+    
+  </div>
+</template>
+<script>
+ export default {
+   data(){
+     return {
+       value10:80,
+       marks: {
+          0: 'start',
+          25: '25%',
+          50: 'center',
+          75: '75%',
+          100: '<i style="color:red">end</i>'
+        }
+     }
+   }
+ }
+</script>
+```
+:::
+
+## API
+### Slider
+|参数|类型|说明|
+|-|-|-|
+|value          | Number,Array   |当前值，默认为0。Array时为范围选择|
+|min      　　　 | Number/0       |最小值|
+|max      　　　 | Number/100     |最大值|
+|step      　　　| Number/1       |步长|
+|disabled       | Boolean/false  |是否禁用|
+|showStops      | Boolean/false  |是否显示间断点|
+|showTooltip　　 | Boolean/false  |是否显示 tooltip|
+|formatTooltip  | Function       |格式化 tooltip message，仅当showTooltip为true时|
+|marks      　　 | Function　　　  |标记， key 的类型必须为number，表示当前位置 |
+
+### Slider Event
+|参数|说明|
+|-|-|
+|change        |值改变时触发|
--- packages/slider/index.vue
+++ packages/slider/index.vue
@@ -1,21 +1,21 @@
 <!-- Created by 337547038 on 2019/11/12 0012. -->
 <template>
   <div :class="{[prefixCls+'-slider']:true,disabled:disabled}">
-    <div :class="`${prefixCls}-slider-runway`">
+    <div :class="`${prefixCls}-slider-runway`" @click="_slierClick">
       <span class="slider-control"
             :style="{left:startLeft+'%'}"
-            @mousedown="_mouseDown('left',$event)">
-        <i v-html="tooltipStart" class="slider-tooltip"></i>
+            @mousedown="_mouseDown('left',$event)" @click.stop="_clickStop">
+          <i v-html="tooltipStart" class="slider-tooltip" v-if="showTooltip"></i>
       </span>
       <span class="slider-control"
             v-if="range"
-            @mousedown="_mouseDown('right',$event)"
+            @mousedown="_mouseDown('right',$event)" @click.stop="_clickStop"
             :style="{left:endLeft+'%'}">
-        <i v-html="tooltipEnd" class="slider-tooltip"></i>
-      </span>
+          <i v-html="tooltipEnd" class="slider-tooltip" v-if="showTooltip"></i>
+        </span>
       <span class="slider-bar" :style="sliderBarStyle"></span>
-      <i class="slider-stop" v-for="(item,index) in sliderStops"
-         :key="index"
+      <i class="slider-stop" v-for="item in sliderStops"
+         :key="item"
          :style="{left:item}"></i>
       <span class="slider-marks" v-for="(item,index) in marksList"
             :key="index"
@@ -30,7 +30,7 @@ import dom from '../mixins/dom.js'
 
 export default {
   name: `${prefixCls}Slider`,
-  data () {
+  data() {
     return {
       prefixCls: prefixCls,
       startLeft: 0,
@@ -69,11 +69,17 @@ export default {
       default: true
     },
     formatTooltip: Function, // 格式化提示信息
-    marks: Object
+    marks: Object,
+    change: Function
   },
   components: {},
+  watch: {
+    value() {
+      this.__init()
+    }
+  },
   methods: {
-    __init () {
+    __init() {
       // 根据初始值定位初始位置
       if (this.range) {
         this._setDefaultValue(this.value[0], 'left')
@@ -83,7 +89,7 @@ export default {
         this._setDefaultValue(this.value, 'left')
       }
     },
-    _setDefaultValue (val, direction) {
+    _setDefaultValue(val, direction) {
       // 控制在最小和最大之间
       let value = val
       if (val < this.min) {
@@ -101,7 +107,7 @@ export default {
         this.endLeft = currentValue
       }
     },
-    _mouseDown (direction, e) {
+    _mouseDown(direction, e) {
       if (this.disabled) {
         return
       }
@@ -115,9 +121,7 @@ export default {
           distance = 100
         }
         // 步长所点比例
-        // const stepPercent = (this.step - this.min) / (this.max - this.min) * 100
-        const stepPercent = this._numberToPercentage(this.step)
-        if (parseInt(distance) % stepPercent === 0) {
+        if (Math.round(distance) % this.getStepPercent === 0) {
           if (typeof this.value === 'object') {
             // 范围选择时
             if (direction === 'left') {
@@ -134,18 +138,27 @@ export default {
         }
       }
       document.onmouseup = () => {
+        // 鼠标弹起时，如果起始值大于终点值，则互换下
+        if (this.startLeft > this.endLeft) {
+          this._emit('change')
+        }
         document.onmousemove = null
         document.onmouseup = null
       }
+      e.preventDefault()
       e.stopPropagation()
     },
-    _emit () {
+    _clickStop(e) {
+      e.preventDefault()
+      e.stopPropagation()
+    },
+    _emit(type) {
       let val = ''
       if (this.range) {
         const min = this._percentageToNumber(this.startLeft)
         const max = this._percentageToNumber(this.endLeft)
         val = [min, max]
-        if (min > max) {
+        if (min > max && type === 'change') {
           val = [max, min]
         }
       } else {
@@ -153,23 +166,49 @@ export default {
         val = this._percentageToNumber(this.startLeft)
       }
       this.$emit('input', val)
+      this.$emit('change', val)
+      this.change && this.change(val)
     },
     // 将具体值转百分比
-    _numberToPercentage (value) {
+    _numberToPercentage(value) {
       // (当前值－最小值)/(最大值-最小值)*100%＝当前值所占的百分比
-      return (value - this.min) / (this.max - this.min) * 100
+      return Math.round((value - this.min) / (this.max - this.min) * 100)
     },
     // 将百分比值转具体值
-    _percentageToNumber (currentValue) {
-      return parseInt((this.max - this.min) * currentValue / 100 + this.min)
+    _percentageToNumber(currentValue) {
+      return Math.round((this.max - this.min) * currentValue / 100 + this.min)
+    },
+    // 点击时滑动互指定位置
+    _slierClick(e) {
+      const offsetLeft = this.getOffset(this.$el).left // 当前元素偏移位置
+      // 当前点击位置所在百分比位置
+      let d = (e.pageX - offsetLeft) / this.elWidth * 100
+      // 定位到当前步数位置*每步所占的百分比
+      let step = Math.round(d / this.step)
+      // 根据步长修改点击的位置，不能出现指定步长外的地方
+      d = step * this.getStepPercent
+      if (this.range) {
+        const offsetStart = Math.abs(d - this.startLeft) // 到起点的距离
+        const offsetEnd = Math.abs(d - this.endLeft) // 到终点的距离
+        if (offsetStart < offsetEnd) {
+          // 表示离起点比较近，则移动左则点
+          this.startLeft = d
+        } else {
+          // 离终点近
+          this.endLeft = d
+        }
+      } else {
+        this.startLeft = d
+      }
+      this._emit()
     }
   },
   computed: {
-    elWidth () {
+    elWidth() {
       // 返回当前可滑动的宽
       return this.$el.offsetWidth
     },
-    range () {
+    range() {
       // 根据值判断是否为范围选择，数组时且长度为2时 为是
       if (typeof this.value === 'object' && this.value.length === 2) {
         return true
@@ -178,7 +217,7 @@ export default {
       }
     },
     // 选中范围开始位置和长度
-    sliderBarStyle () {
+    sliderBarStyle() {
       if (this.range) {
         let startLeft = this.startLeft
         if (this.startLeft > this.endLeft) {
@@ -196,19 +235,20 @@ export default {
       }
     },
     // 计算间断点的位置和个数，返回每个点的位置
-    sliderStops () {
+    sliderStops() {
       let stopList = []
       if (this.showStops) {
-        // (最大值－最小值)/步长＝个数
+        // (最大值－最小值)/步长＝个数，即分多少步
         const num = (this.max - this.min) / this.step
+        // 100/num=每一步所占的比例
         for (let i = 1; i < num; i++) {
-          stopList.push((i / 10) * 100 + '%')
+          stopList.push(i * 100 / num + '%')
         }
       }
       return stopList
     },
     // 格式化返回提示语
-    tooltipStart () {
+    tooltipStart() {
       if (this.showTooltip) {
         const val = this._percentageToNumber(this.startLeft)
         return this.formatTooltip ? this.formatTooltip(val) : val
@@ -216,7 +256,7 @@ export default {
         return ''
       }
     },
-    tooltipEnd () {
+    tooltipEnd() {
       if (this.showTooltip) {
         const val = this._percentageToNumber(this.endLeft)
         return this.formatTooltip ? this.formatTooltip(val) : val
@@ -224,8 +264,12 @@ export default {
         return ''
       }
     },
+    // 获取每步长所占的百分比
+    getStepPercent() {
+      return Math.round(100 / ((this.max - this.min) / this.step))
+    },
     // 转换mark为key,value形式
-    marksList () {
+    marksList() {
       let list = []
       if (this.marks) {
         for (let i in this.marks) {
@@ -238,7 +282,7 @@ export default {
       return list
     }
   },
-  mounted () {
+  mounted() {
     this.__init()
   }
 }
