fdf12c87a8d828edf8f91680a797096e8fdfece5
337547038 <337547038@qq.com>
2021-04-25 18:11:22 +0800

    4.25

---
+++ packages/carousel/ItemList.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: 'itemList',
+  data () {
+    return {}
+  },
+  props: {
+    childItem: Object,
+    styleCls: Object
+  },
+  components: {},
+  methods: {},
+  computed: {},
+  mounted () {
+  },
+  render (h) {
+    let cls = 'carousel-slide'
+    if (this.childItem.className) {
+      cls += ' ' + this.childItem.className
+    }
+    return (<div class={cls} style={this.styleCls}>
+      {this.childItem.renderCell.call(this, h)}</div>)
+  }
+}
+</script>
---
+++ src/views/test/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="aaa">
+    <ak-carousel :loop="true">
+      <ak-carousel-item v-for="item in 6" :key="item">
+        {{item}}
+      </ak-carousel-item>
+      <ak-carousel-item className="tee">afsf</ak-carousel-item>
+    </ak-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {}
+  },
+  props: {},
+  components: {},
+  methods: {},
+  computed: {},
+  mounted () {
+  }
+}
+</script>
