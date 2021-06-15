306f4034f099d907564817108cb4e89314b60eae
337547038 <337547038@qq.com>
2021-06-15 23:41:12 +0800

    add

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,29 +1,36 @@
 <!-- Created by 337547038 on 2021/5/17 0017. -->
 <template>
-  <p>已选值：{{checkbox9}}</p>
+  <p>已选值：{{ checkbox9 }}</p>
   <p>
     <ak-checkbox-group v-model="checkbox9" :options="groupData" :min="2" :max="4" />
   </p>
+<!--  <div>
+    <p v-for="item in groupData" @click="click4(item)">{{ item.label }}-{{ item._disabled }}</p>
+  </div>-->
 </template>
 <script>
-import {reactive, toRefs} from 'vue'
+import {reactive, toRefs, ref} from 'vue'
 
 export default {
   setup() {
     const state = reactive({
       checkbox9: ['a5']
     })
-    const groupData = [
+
+    const groupData = ref([
       {label: '选项1', value: 'a1'},
       {label: '选项2', value: 'a2'},
       {label: '选项3', value: 'a3'},
       {value: 'a4'},
       {label: '选项5', value: 'a5'},
       {label: '选项6', value: 'a6'}
-    ]
+    ])
+    const click4 = (item) => {
+      item.a = new Date().getTime()
+    }
     return {
       ...toRefs(state),
-      groupData
+      groupData, click4
     }
   }
 }
